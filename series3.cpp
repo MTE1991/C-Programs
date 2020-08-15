7eaf26b043b1b62ba588fd99f9439070c7adcaae
MT Ekleel <63772286+MTE1991@users.noreply.github.com>
2020-08-15 09:22:30 +0000

    Update series3.cpp

--- series3.cpp
+++ series3.cpp
@@ -4,12 +4,12 @@ int main(void)
 {
     int i = 0, d=2;
 	
-	printf("The aritmetic series: ");
+    printf("The aritmetic series: ");
 	
-	while (i<=d){
-		i += 2;
-		printf("%d ", i);
-	}
+    while (i<=d){
+	i += 2;
+	printf("%d ", i);
+    }
 	
-	return 0;
+    return 0;
 }
