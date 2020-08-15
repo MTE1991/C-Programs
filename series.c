16b2b42060976912e2b734e9b813bb1b7be7100e
MT Ekleel <63772286+MTE1991@users.noreply.github.com>
2020-08-15 12:43:20 +0600

    Create series.c

---
+++ series.c
@@ -0,0 +1,20 @@
+// Aritmetic finite series
+
+#include <stdio.h>
+
+int main()
+{
+    int i, d; // d is the common difference
+    i = 2;
+    
+    printf("Enter the value of d =");
+    scanf("%d", &n);
+    
+    printf("The arithmetic series: ")
+    do {
+        printf("%d ",i);
+        i+=2;
+    } while (i<=d);
+
+    return 0;
+}
