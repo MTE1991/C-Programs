59196168e2f02a569f926d841702fbb7c5e3a308
MT Ekleel <63772286+MTE1991@users.noreply.github.com>
2020-08-19 10:48:25 +0600

    Update maxof4.c

--- maxof4.c
+++ maxof4.c
@@ -2,14 +2,14 @@
 
 int max_of_four(int a, int b, int c, int d) 
 {
- int max = 0;
-    
- if(max <= a) max = a;
- if(max <= b) max = b;
- if(max <= c) max = c;
- if(max <= d) max = d;
- 
- return max;   
+    int max = 0;
+
+    if(max <= a) max = a;
+    if(max <= b) max = b;
+    if(max <= c) max = c;
+    if(max <= d) max = d;
+
+    return max;   
 }
 
 int main()
