030fd12bf5f8bf403ee2901ec4c053a95644dd78
MT Ekleel <63772286+MTE1991@users.noreply.github.com>
2020-09-01 12:37:46 +0600

    Update astro.c

--- astro.c
+++ astro.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <math.h>
 #include <string.h>
 
+#define PI 3.141592654
+#define G 6.673e-11 // Nm^2 kg^-2
 #define C 3e8 // m/s
 #define AU 1.496e11 // m
 #define LY 9.46e15 // m
 #define PARSEC 3.26 // ly
+#define gravity(m, r) ((G * m) / (r * r)) // gravity of any object in macro definition
+#define density(m, v) (m / v) // density in kgm^-3
 
 char planets[9][20] = {
 		"Mercury",
@@ -19,21 +24,25 @@ char planets[9][20] = {
 	};
 
 void solarsys(void);
-double conv_to_m(char unit[], double val);
+double conv_to_m(char unit[], double val); // Convert to metre
+double r_s(double m, double r); // Schwarzchild radius
+double d_s(double m); // Schwarzchild density
 
 int main(void)
 {
 	int ch, ch2; 
 	double val;
 	char unit1[] = "AU", unit2[] = "PARSEC", unit3[] = "LY";
-
+	printf("Note: use SI units when inputting unit values other than AU, LY and PARSEC.\n\n");
 	printf("1. Planets distance from sun\n");
 	printf("2. Astro-distance converter\n");
+	printf("3. Schwarzchild radius and density of any object\n");
+	printf("4. Gravity of any object\n");
+	printf("5. Density of any object\n");
 	printf("Your choice? > ");
 	scanf("%d", &ch);
 
 	if (ch == 1) solarsys();
-
 	else if (ch == 2) {
 		printf("Choose unit to convert to metre:\n");
 		printf("1. AU\n2. PARSEC\n3.LY (light years)\nYour choice? > ");
@@ -57,7 +66,25 @@ int main(void)
 			printf("%.3lf LY = %.3e m\n", val, conv_to_m(unit3, val));
 		}
 	}
-	
+	else if (ch == 3) {
+		double m, r;
+		printf("Enter the mass and radius: ");
+		scanf("%lf %lf", &m, &r);
+		printf("Schwarzchild radius = %.3lf m\n", r_s(m, r));
+		printf("Schwarzchild density = %.3lf kg/m^3\n", d_s(m));
+	}
+	else if (ch == 4) {
+		double m, r;
+		printf("Enter mass and radius: ");
+		scanf("%lf %lf", &m, &r);
+		printf("g = %.3lf m/s^2\n", gravity(m, r));
+	}
+	else if (ch==5) {
+		double m, v;
+		printf("Enter mass and volume: ");
+		scanf("%lf %lf", &m, &v);
+		printf("Density = %.3e kg/m^3", density(m, v));
+	}
 	return 0;
 }
 
@@ -81,4 +108,14 @@ double conv_to_m(char unit[], double val)
 	if (strcmp(unit, "AU") == 0) return val * AU;
 	else if (strcmp(unit, "PARSEC") == 0) return val * PARSEC * LY;
 	else return val * LY;
+}
+
+double r_s(double m, double r)
+{
+	return (2 * G * m) / pow(C, 2);
+}
+
+double d_s(double m)
+{
+	return (3 * pow(C, 6)) / (32 * PI * pow(G, 3) * pow(m, 2));
 }
